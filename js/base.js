af8dc39c58b873a1e25cd32570eb7ba75e09a849
Kevin Roh <Schrws@users.noreply.github.com>
2020-09-27 13:12:09 +0900

    Add scope

--- js/base.js
+++ js/base.js
@@ -15,5 +15,5 @@ window.addEventListener('resize', () => {
 });
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/js/worker.min.js').then(() => { });
+    navigator.serviceWorker.register('/js/worker.min.js', { scope: '/' }).then(() => { });
 }
